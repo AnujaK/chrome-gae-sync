ddc2d3c318b46b34f67a763b2d65c0b5798498e1
AnujaK <a.for.anuja@gmail.com>
2014-08-04 21:09:50 +0530

    list stores working

---
+++ gae/src/com/gae/sync/entity/Config.java
@@ -0,0 +1,6 @@
+package com.gae.sync.entity;
+
+public class Config {
+    public static final String EMAIL_SCOPE = "https://www.googleapis.com/auth/userinfo.email";
+    public static final String CLIENT_ID = "292824132082.apps.googleusercontent.com";
+}
--- gae/src/com/gae/sync/entity/StoreEndpoint.java
+++ gae/src/com/gae/sync/entity/StoreEndpoint.java
@@ -1,170 +1,168 @@
 package com.gae.sync.entity;
 
-import com.gae.sync.entity.PMF;
-
-import com.google.api.server.spi.config.Api;
-import com.google.api.server.spi.config.ApiMethod;
-import com.google.api.server.spi.config.ApiNamespace;
-import com.google.api.server.spi.response.CollectionResponse;
-import com.google.appengine.api.datastore.Cursor;
-import com.google.appengine.datanucleus.query.JDOCursorHelper;
-
 import java.util.HashMap;
 import java.util.List;
 
 import javax.annotation.Nullable;
 import javax.inject.Named;
-import javax.persistence.EntityExistsException;
-import javax.persistence.EntityNotFoundException;
 import javax.jdo.PersistenceManager;
 import javax.jdo.Query;
+import javax.persistence.EntityNotFoundException;
 
-@Api(name = "storeendpoint", namespace = @ApiNamespace(ownerDomain = "gae.com", ownerName = "gae.com", packagePath = "sync.entity"))
+import com.google.api.server.spi.config.Api;
+import com.google.api.server.spi.config.ApiMethod;
+import com.google.api.server.spi.config.ApiNamespace;
+import com.google.api.server.spi.response.CollectionResponse;
+import com.google.api.server.spi.response.UnauthorizedException;
+import com.google.appengine.api.datastore.Cursor;
+import com.google.appengine.api.users.User;
+import com.google.appengine.datanucleus.query.JDOCursorHelper;
+
+@Api(name = "storeendpoint", namespace = @ApiNamespace(ownerDomain = "bootsimply.com", ownerName = "bootsimply.com", packagePath = "sync.entity"))
 public class StoreEndpoint {
 
-	/**
-	 * This method lists all the entities inserted in datastore.
-	 * It uses HTTP GET method and paging support.
-	 *
-	 * @return A CollectionResponse class containing the list of all entities
-	 * persisted and a cursor to the next page.
-	 */
-	@SuppressWarnings({ "unchecked", "unused" })
-	@ApiMethod(name = "listStore")
-	public CollectionResponse<Store> listStore(
-			@Nullable @Named("cursor") String cursorString,
-			@Nullable @Named("limit") Integer limit) {
+    /**
+     * This method lists all the entities inserted in datastore. It uses HTTP GET method and paging support.
+     * 
+     * @return A CollectionResponse class containing the list of all entities persisted and a cursor to the next page.
+     */
+    @SuppressWarnings({ "unchecked", "unused" })
+    @ApiMethod(name = "listStore")
+    public CollectionResponse<Store> listStore(@Nullable @Named("cursor") String cursorString, @Nullable @Named("limit") Integer limit) {
 
-		PersistenceManager mgr = null;
-		Cursor cursor = null;
-		List<Store> execute = null;
+	PersistenceManager mgr = null;
+	Cursor cursor = null;
+	List<Store> execute = null;
 
-		try {
-			mgr = getPersistenceManager();
-			Query query = mgr.newQuery(Store.class);
-			if (cursorString != null && cursorString != "") {
-				cursor = Cursor.fromWebSafeString(cursorString);
-				HashMap<String, Object> extensionMap = new HashMap<String, Object>();
-				extensionMap.put(JDOCursorHelper.CURSOR_EXTENSION, cursor);
-				query.setExtensions(extensionMap);
-			}
+	try {
+	    mgr = getPersistenceManager();
+	    Query query = mgr.newQuery(Store.class);
+	    if (cursorString != null && cursorString != "") {
+		cursor = Cursor.fromWebSafeString(cursorString);
+		HashMap<String, Object> extensionMap = new HashMap<String, Object>();
+		extensionMap.put(JDOCursorHelper.CURSOR_EXTENSION, cursor);
+		query.setExtensions(extensionMap);
+	    }
 
-			if (limit != null) {
-				query.setRange(0, limit);
-			}
+	    if (limit != null) {
+		query.setRange(0, limit);
+	    }
 
-			execute = (List<Store>) query.execute();
-			cursor = JDOCursorHelper.getCursor(execute);
-			if (cursor != null)
-				cursorString = cursor.toWebSafeString();
+	    execute = (List<Store>) query.execute();
+	    cursor = JDOCursorHelper.getCursor(execute);
+	    if (cursor != null)
+		cursorString = cursor.toWebSafeString();
 
-			// Tight loop for fetching all entities from datastore and accomodate
-			// for lazy fetch.
-			for (Store obj : execute)
-				;
-		} finally {
-			mgr.close();
-		}
-
-		return CollectionResponse.<Store> builder().setItems(execute)
-				.setNextPageToken(cursorString).build();
+	    // Tight loop for fetching all entities from datastore and accomodate
+	    // for lazy fetch.
+	    for (Store obj : execute)
+		;
+	} finally {
+	    mgr.close();
 	}
 
-	/**
-	 * This method gets the entity having primary key id. It uses HTTP GET method.
-	 *
-	 * @param id the primary key of the java bean.
-	 * @return The entity with primary key id.
-	 */
-	@ApiMethod(name = "getStore")
-	public Store getStore(@Named("id") Long id) {
-		PersistenceManager mgr = getPersistenceManager();
-		Store store = null;
-		try {
-			store = mgr.getObjectById(Store.class, id);
-		} finally {
-			mgr.close();
-		}
-		return store;
+	return CollectionResponse.<Store> builder().setItems(execute).setNextPageToken(cursorString).build();
+    }
+
+    /**
+     * This method gets the entity having primary key id. It uses HTTP GET method.
+     * 
+     * @param id
+     *            the primary key of the java bean.
+     * @return The entity with primary key id.
+     */
+    @ApiMethod(name = "getStore")
+    public Store getStore(@Named("id") Long id) {
+	PersistenceManager mgr = getPersistenceManager();
+	Store store = null;
+	try {
+	    store = mgr.getObjectById(Store.class, id);
+	} finally {
+	    mgr.close();
+	}
+	return store;
+    }
+
+    /**
+     * This inserts a new entity into App Engine datastore. If the entity already exists in the datastore, an exception is thrown. It uses HTTP POST
+     * method.
+     * 
+     * @param store
+     *            the entity to be inserted.
+     * @return The inserted entity.
+     * @throws UnauthorizedException
+     */
+    @ApiMethod(name = "insertStore", scopes = { Config.EMAIL_SCOPE }, clientIds = { Config.CLIENT_ID })
+    public Store insertStore(Store store, User user) throws UnauthorizedException {
+	if (user == null) {
+	    throw new UnauthorizedException("UnauthorizedException # User is Null.");
+	} else if (store == null || store.getData() == null || store.getId() != null) {
+	    return null;
 	}
 
-	/**
-	 * This inserts a new entity into App Engine datastore. If the entity already
-	 * exists in the datastore, an exception is thrown.
-	 * It uses HTTP POST method.
-	 *
-	 * @param store the entity to be inserted.
-	 * @return The inserted entity.
-	 */
-	@ApiMethod(name = "insertStore")
-	public Store insertStore(Store store) {
-		PersistenceManager mgr = getPersistenceManager();
-		try {
-			
-			if (containsStore(store)) {
-				throw new EntityExistsException("Object already exists");
-			}
-			mgr.makePersistent(store);
-		} finally {
-			mgr.close();
-		}
-		return store;
+	PersistenceManager mgr = getPersistenceManager();
+	try {
+	    mgr.makePersistent(store);
+	} finally {
+	    mgr.close();
 	}
+	return store;
+    }
 
-	/**
-	 * This method is used for updating an existing entity. If the entity does not
-	 * exist in the datastore, an exception is thrown.
-	 * It uses HTTP PUT method.
-	 *
-	 * @param store the entity to be updated.
-	 * @return The updated entity.
-	 */
-	@ApiMethod(name = "updateStore")
-	public Store updateStore(Store store) {
-		PersistenceManager mgr = getPersistenceManager();
-		try {
-			if (!containsStore(store)) {
-				throw new EntityNotFoundException("Object does not exist");
-			}
-			mgr.makePersistent(store);
-		} finally {
-			mgr.close();
-		}
-		return store;
+    /**
+     * This method is used for updating an existing entity. If the entity does not exist in the datastore, an exception is thrown. It uses HTTP PUT
+     * method.
+     * 
+     * @param store
+     *            the entity to be updated.
+     * @return The updated entity.
+     */
+    @ApiMethod(name = "updateStore")
+    public Store updateStore(Store store) {
+	PersistenceManager mgr = getPersistenceManager();
+	try {
+	    if (!containsStore(store)) {
+		throw new EntityNotFoundException("Object does not exist");
+	    }
+	    mgr.makePersistent(store);
+	} finally {
+	    mgr.close();
 	}
+	return store;
+    }
 
-	/**
-	 * This method removes the entity with primary key id.
-	 * It uses HTTP DELETE method.
-	 *
-	 * @param id the primary key of the entity to be deleted.
-	 */
-	@ApiMethod(name = "removeStore")
-	public void removeStore(@Named("id") Long id) {
-		PersistenceManager mgr = getPersistenceManager();
-		try {
-			Store store = mgr.getObjectById(Store.class, id);
-			mgr.deletePersistent(store);
-		} finally {
-			mgr.close();
-		}
+    /**
+     * This method removes the entity with primary key id. It uses HTTP DELETE method.
+     * 
+     * @param id
+     *            the primary key of the entity to be deleted.
+     */
+    @ApiMethod(name = "removeStore")
+    public void removeStore(@Named("id") Long id) {
+	PersistenceManager mgr = getPersistenceManager();
+	try {
+	    Store store = mgr.getObjectById(Store.class, id);
+	    mgr.deletePersistent(store);
+	} finally {
+	    mgr.close();
 	}
+    }
 
-	private boolean containsStore(Store store) {
-		PersistenceManager mgr = getPersistenceManager();
-		boolean contains = true;
-		try {
-			mgr.getObjectById(Store.class, store.getId());
-		} catch (javax.jdo.JDOObjectNotFoundException ex) {
-			contains = false;
-		} finally {
-			mgr.close();
-		}
-		return contains;
+    private boolean containsStore(Store store) {
+	PersistenceManager mgr = getPersistenceManager();
+	boolean contains = true;
+	try {
+	    mgr.getObjectById(Store.class, store.getId());
+	} catch (javax.jdo.JDOObjectNotFoundException ex) {
+	    contains = false;
+	} finally {
+	    mgr.close();
 	}
+	return contains;
+    }
 
-	private static PersistenceManager getPersistenceManager() {
-		return PMF.get().getPersistenceManager();
-	}
+    private static PersistenceManager getPersistenceManager() {
+	return PMF.get().getPersistenceManager();
+    }
 
 }
