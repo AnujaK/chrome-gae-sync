eb50689b3773bbaa2f3a78c4badbfe9c2ce1a72d
AnujaK <a.for.anuja@gmail.com>
2014-08-05 22:01:44 +0530

    Sample chrome app

--- sample/main.js
+++ sample/main.js
@@ -1,1 +1,3 @@
-//js file
+chrome.app.runtime.onLaunched.addListener(function() {
+  chrome.app.window.create('index.html', {"id": "ChromeGaeSync", "bounds": { "width": 1024, "height": 768 } });
+});
